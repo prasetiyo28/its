425d4b7b6adb9f1ce78c1430872df9ce4137924b
Bayu Adi Prasetiyo <bayu@leson.id>
2020-01-11 23:39:45 +0700

    fixing bug overflow page in login

--- lib/forms.dart
+++ lib/forms.dart
@@ -96,6 +96,7 @@ class Datas {
 class Forms extends StatefulWidget {
   @override
   _FormsState createState() => _FormsState();
+  
 }
 
 class _FormsState extends State<Forms> {
@@ -109,6 +110,8 @@ class _FormsState extends State<Forms> {
   TextEditingController lastDateController = TextEditingController();
   String buttonText = "Kapan Tanggal terakhir pengiriman barang ?";
   // TextEditingController documentController = TextEditingController();
+
+  
 
   DateTime selectedDate = DateTime.now();
   var formatter = new DateFormat('yyyy-MM-dd');
@@ -131,6 +134,8 @@ class _FormsState extends State<Forms> {
   Uang _selectedUang;
   Pembayaran _selectedPembayaran;
 
+  
+  
   @override
   void initState() {
     
@@ -244,6 +249,9 @@ class _FormsState extends State<Forms> {
 
   @override
   Widget build(BuildContext context) {
+
+  
+  
     Color green = const Color(0xff2C918D);
     return Scaffold(
       body: Container(
@@ -522,36 +530,323 @@ class _FormsState extends State<Forms> {
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Alert Dialog title"),
-          content: new Text("Alert Dialog body"),
+          backgroundColor: const Color(0xff2C918D),
+          title: new Text("Incoterms ?",style: TextStyle(color: Colors.white),),
+          content: Container(child: new Text("Incoterms menjelaskan hak dan kewajiban pembeli dan penjual yang berhubungan dengan pengiriman barang seperti biaya dan tanggung jawab atas barang.",style: TextStyle(color: Colors.white),)),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _showDialogs();
-              },
+            ButtonTheme(
+              
+              buttonColor: const Color(0xffB8E6DB),
+                          child: new RaisedButton(
+                            shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.white)),
+                child: new Text("Lanjut"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _showIncoterms();
+                },
+              ),
             ),
+
+            
           ],
         );
       },
     );
   }
 
-  void _showDialogs() {
+  void _showDetailIncoterms(text) {
+    var judul = '';
+    var contents = '';
+
+    if (text == 'EXW') {
+    judul = 'EXW - Ex Works';
+    contents = 'Pihak penjual menentukan tempat pengambilan barang, Pihak pembeli bertanggung jawab untuk biaya angkut, resiko selama perjalanan dan biaya saat pembongkaran.';
+  }else if(text == 'FCA'){
+    judul = 'FCA - Free Carrier ';
+    contents = 'Pihak penjual hanya bertanggung jawab untuk mengurus izin ekspor dan meyerahkan barang ke pihak pengangkut di tempat yang telah ditentukan. Pihak pembeli hanya membayar biaya pengiriman dan tanggung jawab hangus saat barang di serahkan di pelabuhan tujuan.';
+  }else if(text == 'CPT'){
+    judul = 'CPT - Carriage Paid To';
+    contents = 'Pihak penjual menanggung biaya sampai barang tiba di tempat tujuan, namun tanggung jawab hanya sampai saat barang diserahkan ke pihak pengangkut.';
+  }else if(text == 'CPT'){
+    judul = 'CPT - Carriage Paid To';
+    contents = 'Pihak penjual menanggung biaya sampai barang tiba di tempat tujuan, namun tanggung jawab hanya sampai saat barang diserahkan ke pihak pengangkut.';
+  }else if(text == 'CIP'){
+    judul = 'CIP - Carriage and Insurance Paid to';
+    contents = 'Sama seperti CPT ditambah pihak penjual wajib membayar asuransi untuk barang yang dikirim hingga barang diserahkan.';
+  }else if(text == 'CPT'){
+    judul = 'CPT - Carriage Paid To';
+    contents = 'Pihak penjual menanggung biaya sampai barang tiba di tempat tujuan, namun tanggung jawab hanya sampai saat barang diserahkan ke pihak pengangkut.';
+  }
+  else if(text == 'CPT'){
+    judul = 'CPT - Carriage Paid To';
+    contents = 'Pihak penjual menanggung biaya sampai barang tiba di tempat tujuan, namun tanggung jawab hanya sampai saat barang diserahkan ke pihak pengangkut.';
+  }
+  else if(text == 'DAT'){
+    judul = 'DAT – Delivered at Terminal';
+    contents = 'Pihak penjual membayar sampai ke pembongkaran di terminal pelabuhan tujuan, kecuali beban biaya sehubungan biaya, tanggung jawab bebas saat kapal selesai bongkar di terminal pelabuhan tujuan.';
+  }
+  else if(text == 'DAP'){
+    judul = 'DAP – Delivered at Place';
+    contents = 'Hampir sama sepertiDAT dengan tambahan biaya pengangkutan ke tempat tujuan dan asuransi menjadi tanggaungan pihak penjual';
+  }
+  else if(text == 'DDP'){
+    judul = 'DDP - Delivered Duty Paid';
+    contents = 'Pihak penjual bertanggung jawab mengantar barang sampai di tempat tujuan, termasuk biaya asuransi dan semua biaya lain yang mungkin muncul sebagai biaya impor, cukai dan pajak dari negara pihak pembeli. Izin impor juga menjadi tanggung jawab pihak penjual.';
+  }
+  else if(text == 'FAS'){
+    judul = 'FAS - Free Alongside Ship';
+    contents = 'Pihak penjual bertanggung jawab sampai barang berada di pelabuhan keberangkatan dan siap disamping kapal untuk dimuat. Biaya lain samapai ke tempat tujuan akan menjadi tanggung jawab pihak pembeli. Hanya berlaku untuk transportasi air.';
+  }
+  else if(text == 'FOB'){
+    judul = 'FOB - Free On Board';
+    contents = 'Pihak penjual bertanggung jawab dari mengurus izin ekspor sampai memuat barang di kapal yang siap berangkat. Biaya pengangkutan dari pelabuhan asal samapi ke tempat tujuan akan menjadi tanggungan pembeli. Hanya berlaku untuk transportasi air.';
+  }
+  else if(text == 'CFR'){
+    judul = 'CFR - Cost and Freight';
+    contents = 'pihak penjual menanggung biaya sampai kapal yang memuat barang merapat di pelabuhan tujuan, namun tanggung jawab penjual hanya sampai saat barang selesai di muat ke kapal. Hanya berlaku untuk transportasi air.';
+  }
+  else{
+    judul = 'CIF - Cost, Insurance and Freight';
+    contents = 'Sama seperti CFR ditambah pihak penjual wajib membayar asuransi untuk barang yang dikirim. Hanya berlaku untuk transportasi air.';
+  }
     // flutter defined function
     showDialog(
       context: context,
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Dialog Ke 2"),
-          content: new Text("Alert Dialog body"),
+          backgroundColor: const Color(0xff2C918D),
+          title: new Text("Incoterms\n" + judul,style: TextStyle(color: Colors.white),),
+          content: Container(child: new Text("Incoterms menjelaskan hak dan kewajiban pembeli dan penjual yang berhubungan dengan pengiriman barang seperti biaya dan tanggung jawab atas barang.",style: TextStyle(color: Colors.white),)),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Close"),
+            ButtonTheme(
+              
+              buttonColor: const Color(0xffB8E6DB),
+                          child: new RaisedButton(
+                            shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.white)),
+                child: new Text("Lanjut"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _showIncoterms();
+                },
+              ),
+            ),
+
+            
+          ],
+        );
+      },
+    );
+  }
+
+  void _showIncoterms() {
+    Color ijoMuda = const Color(0xffB8E6DB);
+    // flutter defined function
+    showDialog(
+      
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          backgroundColor: const Color(0xff2C918D),
+          // title: new Text("Dialog Ke 2"),
+          content: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                
+                children: <Widget>[
+                  SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("EXW"),onPressed: (){
+                         var text = 'EXW';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: SizedBox(
+                      width: 70,
+                      child: RaisedButton(
+                        color: ijoMuda,
+                         shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                         child: Text("FAS"),onPressed: (){
+                           var text = 'FAS';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                         },)
+                      ),
+                    ),
+
+                    SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("FOB"),onPressed: (){
+                         var text = 'FOB';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+                ],
+              ),
+
+              Row(
+                
+                children: <Widget>[
+                  SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("FCA"),onPressed: (){
+                         var text = 'FCA';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: SizedBox(
+                      width: 70,
+                      child: RaisedButton(
+                        color: ijoMuda,
+                         shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                         child: Text("CRF"),onPressed: (){
+                           var text = 'CRF';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                         },)
+                      ),
+                    ),
+
+                    SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("CPT"),onPressed: (){
+                         var text = 'CPT';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+                ],
+              ),
+
+              Row(
+                
+                children: <Widget>[
+                  SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("CIF"),onPressed: (){
+                         var text = 'CIF';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: SizedBox(
+                      width: 70,
+                      child: RaisedButton(
+                        color: ijoMuda,
+                         shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                         child: Text("CIP"),onPressed: (){
+                           var text = 'CIP';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                         },)
+                      ),
+                    ),
+
+                    SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("DAT"),onPressed: (){
+                         var text = 'DAT';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+                ],
+              ),
+
+              Row(
+                
+                children: <Widget>[
+                  SizedBox(
+                    width: 70,
+                    child: RaisedButton(
+                      color: ijoMuda,
+                       shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                       child: Text("DAP"),onPressed: (){
+                         var text = 'DAP';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                       },)
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: SizedBox(
+                      width: 70,
+                      child: RaisedButton(
+                        color: ijoMuda,
+                         shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0)),
+                         child: Text("DDP"),onPressed: (){
+                           var text = 'DDP';
+                         Navigator.of(context).pop();
+                         _showDetailIncoterms(text);
+                         },)
+                      ),
+                    ),
+
+                
+                ],
+              ),
+              
+            ],
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new RaisedButton(
+              color: const Color(0xff2C918D),
+              shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.white)),
+              child: new Text("Close",style: TextStyle(color: Colors.white),),
               onPressed: () {
                 Navigator.of(context).pop();
               },
--- lib/login.dart
+++ lib/login.dart
@@ -26,108 +26,112 @@ class _LoginState extends State<Login> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-          child: Stack(
-        children: <Widget>[
-          Center(
-            child: new Image.asset(
-              'assets/bg.jpeg',
-              width: size.width,
-              height: size.height,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30 , bottom: 30, left: 20, right: 20),
-            child: Center(
-              child: Container(
-                width: size.width,
-                height: size.height,
-                color: green,
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "Ekspor\nSimulator",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                      textAlign: TextAlign.center,
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 100, right: 20, left: 20),
-                      child: Container(
-                        width: size.width,
-                        color: Colors.white,
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Selamat Datang",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 10, left: 20, right: 20),
-                              child: TextField(
-                                controller: firstNameController,
-                                style: new TextStyle(
-                                  height: 0.2
-                                ),
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'Nama Depan'),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 8, left: 20, right: 20),
-                              child: TextField(
-                                controller: lastNameController,
-                                style: new TextStyle(
-                                  height: 0.2
-                                ),
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'Nama Belakang'),
-                              ),
-                              
-                            ),
-                            Text("Mari Menjadi Exsportir"),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 20,bottom: 30),
-                              child: ButtonTheme(
-                                  buttonColor: green,
-                                  child: RaisedButton(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(18.0),
-                                        side: BorderSide(color: Colors.white)),
-                                    child: Text(
-                                      "Masukan",
-                                      style: TextStyle(
-                                          fontSize: 20, color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      _navigateHome(context);
-                                    },
-                                  )),
-                            )
-                          ],
-                        ),
-                      ),
-                      
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30),
-                      child: Text("Dibuat Oleh :\nNatan,Sausan,Tresno,Afif",textAlign: TextAlign.center,),
-                    )
-                  ],
+          child: ListView(
+                      children: <Widget>[
+        Stack(
+          children: <Widget>[
+              Center(
+                child: new Image.asset(
+                  'assets/bg.jpeg',
+                  width: size.width,
+                  height: size.height,
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ),
-        ],
-      )),
+              Padding(
+                padding: const EdgeInsets.only(top: 30 , bottom: 30, left: 20, right: 20),
+                child: Center(
+                  child: Container(
+                    width: size.width,
+                    height: size.height,
+                    color: green,
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Ekspor\nSimulator",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(top: 100, right: 20, left: 20),
+                          child: Container(
+                            width: size.width,
+                            color: Colors.white,
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Selamat Datang",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 10, left: 20, right: 20),
+                                  child: TextField(
+                                    controller: firstNameController,
+                                    style: new TextStyle(
+                                      height: 0.2
+                                    ),
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        hintText: 'Nama Depan'),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 8, left: 20, right: 20),
+                                  child: TextField(
+                                    controller: lastNameController,
+                                    style: new TextStyle(
+                                      height: 0.2
+                                    ),
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        hintText: 'Nama Belakang'),
+                                  ),
+                                  
+                                ),
+                                Text("Mari Menjadi Exsportir"),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 20,bottom: 30),
+                                  child: ButtonTheme(
+                                      buttonColor: green,
+                                      child: RaisedButton(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                new BorderRadius.circular(18.0),
+                                            side: BorderSide(color: Colors.white)),
+                                        child: Text(
+                                          "Masukan",
+                                          style: TextStyle(
+                                              fontSize: 20, color: Colors.white),
+                                        ),
+                                        onPressed: () {
+                                          _navigateHome(context);
+                                        },
+                                      )),
+                                )
+                              ],
+                            ),
+                          ),
+                          
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30),
+                          child: Text("Dibuat Oleh :\nNatan,Sausan,Tresno,Afif",textAlign: TextAlign.center,),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
+          )),
     );
   }
   _navigateHome(BuildContext context) async {
